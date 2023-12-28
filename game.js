8b66078769c41649207c40f29713c4ff2a79ca0f
CodeByBlazej <codebyblazej@gmail.com>
2023-12-28 19:23:12 +0000

    added functionality to Mario

--- game.js
+++ game.js
@@ -8,6 +8,10 @@ kaboom({
 
 const MOVE_SPEED = 120
 const JUMP_FORCE = 360
+const BIG_JUMP_FORCE = 550
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+let isJumping = true
+const FALL_DEATH  = 400
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
@@ -23,7 +27,7 @@ loadSprite('pipe-top-right', 'hj2GK4n.png')
 loadSprite('pipe-bottom-left', 'c1cYSbt.png')
 loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
-scene('game', () => {
+scene('game', ({ score }) => {
   layers(['bg', 'obj', 'ui'], 'obj')
 
   const map = [
@@ -33,7 +37,7 @@ scene('game', () => {
     '                                            ',
     '                                            ',
     '                                            ',
-    '       $   =*=%= $                                     ',
+    '       $ %  =*=%= $                                     ',
     '                                 -+           ',
     '                         ^   ^   ()             ',
     '===================================   ======',
@@ -43,7 +47,7 @@ scene('game', () => {
     width: 20,
     height: 20,
     '=': [sprite('block'), solid()],
-    '$': [sprite('coin')],
+    '$': [sprite('coin'), 'coin'],
     '%': [sprite('surprise'), solid(), 'coin-surprise'],
     '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
     '}': [sprite('unboxed'), solid()],
@@ -51,30 +55,109 @@ scene('game', () => {
     ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
     '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
     '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
-    '^': [sprite('evil-shroom'), solid()],
-    '#': [sprite('mushroom'), solid()],
+    '^': [sprite('evil-shroom'), solid(), body(), 'dangerous'],
+    '#': [sprite('mushroom'), solid(), 'mushroom', body()],
   }
 
   const gameLevel = addLevel(map, levelCfg)
 
   const scoreLabel = add([
-    text('test'),
+    text(score),
     pos(30, 6),
     layer('ui'),
     {
-      value: 'test',
+      value: score,
     }
   ])
 
   add([text('level ' + 'test', pos(4,6))])
 
+  function big() {
+    let timer = 0
+    let isBig = false
+    return {
+      update() {
+        if (isBig) {
+          timer -= dt()
+          if (timer <= 0) {
+            this.smallify()
+          }
+        }
+      },
+      isBig() {
+        return isBig
+      },
+      smallify() {
+        this.scale = vec2(1)
+        CURRENT_JUMP_FORCE = JUMP_FORCE
+        timer = 0
+        isBig = false
+      },
+      biggify(time) {
+        this.scale = vec2(2)
+        CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+        timer = time
+        isBig = true
+      }
+    }
+  }
+
   const player = add([
     sprite('mario'), solid(),
     pos(30, 0),
     body(),
+    big(),
     origin('bot')
   ])
 
+  action('mushroom', (m) => {
+    m.move(20, 0)
+  }) 
+
+  player.on('headbump', (obj) => {
+    if (obj.is('coin-surprise')) {
+      gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn('}', obj.gridPos.sub(0,0))
+    }
+        if (obj.is('mushroom-surprise')) {
+      gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn('}', obj.gridPos.sub(0,0))
+    }
+  })
+
+  player.collides('mushroom', (m) => {
+    destroy(m)
+    player.biggify(6)
+  }) 
+
+  player.collides('coin', (c) => {
+    destroy(c)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
+
+  const ENEMY_SPEED = 20
+
+  action('dangerous', (d) => {
+    d.move(-ENEMY_SPEED, 0)
+  })
+
+  player.collides('dangerous', (d) => {
+    if(isJumping) {
+      destroy(d)
+    } else {
+      go('lose', { score: scoreLabel.value})
+    }
+  })
+
+  player.action(() => {
+    camPos(player.pos)
+    if (player.pos.y >= FALL_DEATH) {
+      go('lose', { score: scoreLabel.value})
+    }
+  })
 
   keyDown('left', () => {
     player.move(-MOVE_SPEED, 0)
@@ -84,13 +167,22 @@ scene('game', () => {
     player.move(MOVE_SPEED, 0)
   })
 
-  keyPress('space', () => {
+  player.action(() => {
     if(player.grounded()) {
-      player.jump(JUMP_FORCE)
+      isJumping = false
     }
   })
 
-
+  keyPress('space', () => {
+    if(player.grounded()) {
+      isJumping = true
+      player.jump(CURRENT_JUMP_FORCE)
+    }
+  })
 })
 
-start('game')
+scene('lose', ({ score }) => {
+  add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
+})
+
+start('game', { score: 0})
